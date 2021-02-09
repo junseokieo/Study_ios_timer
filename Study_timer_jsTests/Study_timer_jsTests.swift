1e86c29ac103941df700ce2e795757a5cdcd018b
junseokieo <ccobaltbluee@gmail.com>
2021-02-10 01:11:26 +0900

    Initial Commit

---
+++ Study_timer_jsTests/Study_timer_jsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Study_timer_jsTests.swift
+//  Study_timer_jsTests
+//
+//  Created by 이준석 on 2021/02/10.
+//
+
+import XCTest
+@testable import Study_timer_js
+
+class Study_timer_jsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
