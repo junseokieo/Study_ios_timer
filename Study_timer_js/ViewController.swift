2630715f1c282b0acf0cd4b9d96640c4b37c6587
junseokieo <ccobaltbluee@gmail.com>
2021-02-10 19:17:20 +0900

    2/10 first commit, 1주차

--- Study_timer_js/ViewController.swift
+++ Study_timer_js/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label_hello: UILabel!
+    @IBOutlet weak var Text_num: UITextField!
+    @IBOutlet weak var Text_name: UITextField! 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func Button_click(_ sender: UIButton) {
+        //Label_hello.text = "Button clicked !!"
+        // 버튼을 클릭했을시 학번, 이름이 나오도록.
+        Label_hello.text = "학번 : " + Text_num.text! + "\n이름: " + Text_name.text!
+        
+    }
 
 }
 
