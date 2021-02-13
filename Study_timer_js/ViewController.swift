fa591f0d3dc06e17b128763d04d9f7c8111e5427
junseokieo <ccobaltbluee@gmail.com>
2021-02-14 01:29:07 +0900

    2/14일 커밋_2주차 진행중

--- Study_timer_js/ViewController.swift
+++ Study_timer_js/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Focus_percent: UILabel!
+    @IBOutlet weak var Label_alltime: UILabel!
+    @IBOutlet weak var Label_stackedTime: UILabel!
+    @IBOutlet weak var Label_timer: UILabel!
+    
+    @IBOutlet weak var Button_Start_Outlet: UIButton!
+    @IBOutlet weak var Button_Stop_Outlet: UIButton!
+    @IBOutlet weak var Button_Restart_Outlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button_Start_Action(_ sender: UIButton) {
+    }
+    @IBAction func Button_Stop_Action(_ sender: UIButton) {
+    }
+    @IBAction func Button_Restart_Action(_ sender: UIButton) {
+    }
+    @IBAction func Button_Reset_Action(_ sender: UIButton) {
+    }
+    @IBAction func Button_Timeset_Action(_ sender: UIButton) {
+    }
 }
 
