f3b249bed49b9399f439ceb26066d856da6d8966
junseokieo <ccobaltbluee@gmail.com>
2021-02-16 18:09:04 +0900

    2/16 2주차 완료

--- Study_timer_js/ViewController.swift
+++ Study_timer_js/ViewController.swift
@@ -18,20 +18,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button_Stop_Outlet: UIButton!
     @IBOutlet weak var Button_Restart_Outlet: UIButton!
     
+    
+    // 화면에 들어왔을시 보이는 화면
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Button_Start_Outlet.layer.cornerRadius=8
+        Button_Stop_Outlet.layer.cornerRadius=8
+        Button_Restart_Outlet.layer.cornerRadius=8
     }
 
     @IBAction func Button_Start_Action(_ sender: UIButton) {
+        Label_timer.text = "시작"
     }
     @IBAction func Button_Stop_Action(_ sender: UIButton) {
+        Label_timer.text = "종료"
     }
     @IBAction func Button_Restart_Action(_ sender: UIButton) {
+        Label_timer.text = "재시작"
     }
     @IBAction func Button_Reset_Action(_ sender: UIButton) {
+        Label_timer.text = "리셋"
     }
     @IBAction func Button_Timeset_Action(_ sender: UIButton) {
+        Label_timer.text = "시간설정"
     }
 }
 
