1ff00c2dc819d1031acd02316483c37f9480cbcb
junseokieo <ccobaltbluee@gmail.com>
2021-03-07 23:35:31 +0900

    time setting page(to 15:00)

---
+++ Study_timer_js/setTime_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  setTime_ViewController.swift
+//  Study_timer_js
+//
+//  Created by 이준석 on 2021/03/07.
+//
+
+import UIKit
+
+class setTime_ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label_alltime: UILabel!
+    @IBOutlet weak var label_timer: UILabel!
+    @IBOutlet weak var text_h1: UITextField!
+    @IBOutlet weak var text_m1: UITextField!
+    @IBOutlet weak var text_h2: UITextField!
+    @IBOutlet weak var text_m2: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func button_set(_ sender: UIButton) {
+    }
+    
+
+}
